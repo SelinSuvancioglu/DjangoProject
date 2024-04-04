406b846acd00d5bce9cb5fd32866b7e53bfb3929
Selin Suvancıoğlu <120595449+SelinSuvancioglu@users.noreply.github.com>
2024-04-04 09:52:05 +0300

    Templates ve URL Router yapıldı

--- MyCv/apps.py
+++ MyCv/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from django.contrib import admin
+from django.urls import path
+from .views import index  # index view'ini ekledik
 
 class MycvConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MyCv'
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),  # Boş bir URL, index view'ına yönlendiriliyor
+]
--- MyCv/views.py
+++ MyCv/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+# contact fonksiyonunu geçici olarak boş bırakabiliriz veya kaldırabiliriz
+# def contact(request):
+#     return render(request, 'contact.html')
